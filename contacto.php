824aaa62ef3c479ad1502e14806780be61aa574a
Drakos <orlando.andaur.c@gmail.com>
2021-10-01 22:41:16 -0300

    update form

--- contacto.php
+++ contacto.php
@@ -6,28 +6,39 @@
     <title>Datos</title>
 </head>
 <body>
-    <h1>Formulario de contacto</h1>
-    <h3>Si deseas ponerte en contacto con nosotros, rellene el formulario y te lo responderemos lo antes posible.<h3>
-    <form action="enviado.php" method="post">
-        <input type="text" name="name" placeholder="Nombre Completo" autofocus="">
-        <br>
-        <input type="text" name="firstname" placeholder="(Firstname)">
-        <br>
-        <input type="email" name="email" placeholder="E-mail">
-        <br>
-        <input type="tel" name="telefono" placeholder="Teléfono">
-        <br>
-        <br>
-        <input type="text" name="ciudad" placeholder="Ciudad">
-        <br>
-        <input type="text" name="pais" placeholder="País">
-        <br>
-        <br>
-        <textarea name="texto" rows="15" cols="50"></textarea>
-        <br>
-        <input type="checkbox" name="casilla"><label>Enviarme una copia</label>
-        
-        <input type="submit" name="aceptar" value="Enviar mensaje">
-    </form>
+    <div>
+        <h1>Formulario de contacto</h1>
+        <h3>Si deseas ponerte en contacto con nosotros, rellene el formulario y te lo responderemos lo antes posible.</h3>
+
+        <form class="form" action="./enviado.php" method="post">
+            <div>
+                <div>
+                    <img src="" alt="" />
+                    <img src="" alt="" />
+                </div>
+                
+                <div>
+                    <div>Obligatorio</div>
+
+                    <div>
+                        <input type="text" name="name" placeholder="Nombre Completo" required />
+                        <input type="text" name="firstname" placeholder="Firstname" required />
+                        <input type="text" type="email" name="email" placeholder="example@gmail.com" required />
+                        <input type="text" name="telefono" placeholder="Teléfono" required />
+                    </div>
+
+                    <div>
+                        <input type="text" name="ciudad" placeholder="Ciudad" required />
+                        <input type="text" name="pais" placeholder="País" required />
+                    </div>
+                </div>
+            </div>
+            
+            <div>
+                <input type="checkbox" name="casilla"><label>Enviarme una copia</label>
+                <input type="submit" value="Enviar mensaje" />
+            </div>
+        </form>
+    </div>
 </body>
 </html>
